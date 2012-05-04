2882941d8e1324616ad0ca81f9d0e03ec120969e
Bo Han <bo.han@umassmed.edu>
2012-05-04 17:53:27 -0400

    this is a first test...

---
+++ BO_TAILER_1/test_first.cpp
@@ -0,0 +1,21 @@
+/************************************************
+*** test_first.cpp                   
+*** Created on: May 4, 2012              
+*** Author: Bo W Han                 
+*** Bo.Han@Umassmed.edu  
+*** Zamore Laboratory
+*** University of Massachusetts Medical School            
+*************************************************/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int main ()
+{
+  cout << "project tailer...\n";
+}
+
+
