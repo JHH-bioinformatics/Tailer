744dfe365629b992a2b346f812da4cefb7119fb2
Bo Han <bo.han@umassmed.edu>
2012-05-06 15:24:09 -0400

    First trial of the "Sequence" idea

---
+++ BO_TAILER_1/Sequence.hpp
@@ -0,0 +1,39 @@
+/************************************************
+
+*************************************************/
+
+#ifndef SEQUENCE_HPP_
+#define SEQUENCE_HPP_
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<deque>
+#include<list>
+#include<array>
+#include<boost/iterator/iterator_concepts.hpp>
+using namespace std;
+using namespace boost_concepts;
+
+template <typename T,
+  template <typename ELEM> class CONT = vector>
+class Sequences
+{
+private:
+  CONT<T> container;
+public:
+  Sequences () {};
+  Sequences (const Sequences&);
+  Sequences& operator=(const Sequences&);
+  bool add2Back (const T&);
+  bool add2Front (const T&);
+  T popBack ();
+  T popFront ();
+  void printSequence (istream&);
+};
+
+
+
+
+#endif /* SEQUENCE_HPP_ */
